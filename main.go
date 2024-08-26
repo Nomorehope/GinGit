5fa7468f256138736b00141c3892121e97b10aaa
Nomorehope <54616900+Nomorehope@users.noreply.github.com>
2024-08-26 16:45:33 +0300

    TODO in string out json

--- main.go
+++ main.go
@@ -1,8 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+var tasks = []string{"task1", "task2", "task3"}
+
+func tasksHandler(ctx *gin.Context) {
+	switch ctx.Request.Method {
+	case "GET":
+		ctx.JSON(http.StatusOK, gin.H{"tasks": tasks})
+	case "POST":
+		var newTask string
+		if err := ctx.BindJSON(&newTask); err != nil {
+			ctx.JSON(http.StatusBadRequest, gin.H{"error": "Неправильный ввод"})
+			return
+		}
+		tasks = append(tasks, newTask)
+		ctx.JSON(http.StatusOK, gin.H{"message": "Задача добавлена"})
+	default:
+		ctx.JSON(http.StatusMethodNotAllowed, gin.H{"error": "Ошибка расопзнавания метода"})
+	}
+}
 
 func main() {
-	fmt.Print("Hi")
 
+	r := gin.Default()
+	r.GET("/tasks", tasksHandler)
+	r.POST("/tasks", tasksHandler)
+	s
+	r.Run(":8080")
 }
